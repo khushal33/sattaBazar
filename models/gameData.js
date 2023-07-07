00bccf1d64e71f6480ed15762ad48c29cb31b1ff
siddhishinde0723 <siddhishinde0723@gmail.com>
2023-07-07 17:34:51 +0530

    added result

--- models/gameData.js
+++ models/gameData.js
@@ -1,36 +1,38 @@
 const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
-    gameStatus:{type:String,enum:["start","stop","reload"],default:"stop"},
-    gameResult:{type:Number,default:0},
-    natepute:{
-        zero:{type:Number,default:0},
-        one:{type:Number,default:0},
-        two:{type:Number,default:0},
-        three:{type:Number,default:0},
-        four:{type:Number,default:0},
-        five:{type:Number,default:0},
-        six:{type:Number,default:0},
-        seven:{type:Number,default:0},
-        eight:{type:Number,default:0},
-        nine:{type:Number,default:0},
-    },
-    akluj:{
-        zero:{type:Number,default:0},
-        one:{type:Number,default:0},
-        two:{type:Number,default:0},
-        three:{type:Number,default:0},
-        four:{type:Number,default:0},
-        five:{type:Number,default:0},
-        six:{type:Number,default:0},
-        seven:{type:Number,default:0},
-        eight:{type:Number,default:0},
-        nine:{type:Number,default:0},
-    }
-})
+  gameStatus: {
+    type: String,
+    enum: ["start", "stop", "reload", "result"],
+    default: "stop",
+  },
+  gameResult: { type: String, default: 0 },
+  natepute: {
+    zero: { type: Number, default: 0 },
+    one: { type: Number, default: 0 },
+    two: { type: Number, default: 0 },
+    three: { type: Number, default: 0 },
+    four: { type: Number, default: 0 },
+    five: { type: Number, default: 0 },
+    six: { type: Number, default: 0 },
+    seven: { type: Number, default: 0 },
+    eight: { type: Number, default: 0 },
+    nine: { type: Number, default: 0 },
+  },
+  akluj: {
+    zero: { type: Number, default: 0 },
+    one: { type: Number, default: 0 },
+    two: { type: Number, default: 0 },
+    three: { type: Number, default: 0 },
+    four: { type: Number, default: 0 },
+    five: { type: Number, default: 0 },
+    six: { type: Number, default: 0 },
+    seven: { type: Number, default: 0 },
+    eight: { type: Number, default: 0 },
+    nine: { type: Number, default: 0 },
+  },
+});
 
+const GameData = mongoose.model("GameData", gameSchema);
 
-
-const GameData = mongoose.model('GameData',gameSchema)
-
-module.exports = GameData
+module.exports = GameData;
--- router/adminTalks.js
+++ router/adminTalks.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth')
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth");
+const { updateGameData } = require("../controllers/gameData");
 
+router.post("/message", authController.verifyToken, async (req, res) => {
+  const { message } = req.body;
+  // authController.signIn(req).then((user)=>{
 
-router.post('/message',authController.verifyToken,async (req,res)=>{
-    const {message} = req.body
-    // authController.signIn(req).then((user)=>{
-        io.emit("message",message)
-    res.status(200).send(message)
-    // }).catch((err)=>{
-    //     res.status(400).send(err)
-    // })
-})
+  let req1 = {
+    gameStatus: "result",
+    gameResult: message,
+  };
+  await updateGameData({ body: req1 }, {}).catch((err) => {
+    console.log("reeeeeeeeeeeeeeeeee", err);
+  });
+  io.emit("message", message);
+  res.status(200).send(message);
+  // }).catch((err)=>{
+  //     res.status(400).send(err)
+  // })
+});
 
 module.exports = router;
