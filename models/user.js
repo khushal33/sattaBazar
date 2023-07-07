2552817d3265ddd3f5566f63bfa51471bf3a426d
Khushal33 <khuhsalpatil033@gmail.com>
2023-07-07 21:48:20 +0530

    issue fixe

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email:{type:String,required:true},
     name:{type:String,required:true},
     role:{type:String,required:true,enum:['admin','user']},
-    branchName:{type:String,required:true,enum:['admin','natepute','akluj']},
+    branchName:{type:String,required:true},
     password:{type:String,required:true}
 })
 
